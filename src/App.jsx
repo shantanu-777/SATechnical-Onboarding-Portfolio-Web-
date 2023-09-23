0c6271f5d299b8b4933d150e45d25b13e6429ae0
shantanu-777 <shantanu.modhave2021@vitbhopal.ac.in>
2023-09-23 15:04:28 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -24,17 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
-};
+  name: "Shantanu Modhave",
+  title: "Competitive coder And Data Analyst",
+  email: "shantanumodhave7@gmail.com",
+  gitHub: "shantanu-7777",
+  instagram: "shantanu.modhave",
+  linkedIn: "shantanu modhave"
 
+};
 
 const primaryColor = "#4E567E";
 const secondaryColor = "#D2F1E4";
